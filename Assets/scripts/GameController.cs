69b3a1303ae0e19e18fcfbce5582291bfe71dd13
pavani-kommana <80096838+pavani-kommana@users.noreply.github.com>
2023-02-22 21:49:52 +0530

    updated game manager

---
+++ Assets/scripts/GameController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameController : MonoBehaviour
+{
+    public List<Button> btn = new List<Button> ();
+    // Start is called before the first frame update
+
+    public void GetButton()
+    {
+        GameObject[] obj = GameObject.FindGameObjectsWithTag("puzzleButton");
+        for(int i = 0;i< obj.Length; i++)
+        {
+           // btn.Add(obj[i].GetComponents<Button>);
+        }
+    }
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/scripts/GameManager.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour
+{
+    public List<Button> btn = new List<Button>();
+    // Start is called before the first frame update
+
+    private void Start()
+    {
+        GetButton();
+    }
+    public void GetButton()
+    {
+        GameObject[] obj = GameObject.FindGameObjectsWithTag("puzzleButton");
+        for (int i = 0; i < obj.Length; i++)
+        {
+            btn.Add(obj[i].GetComponent<Button>());
+            // Update is called once per frame
+        }
+}
