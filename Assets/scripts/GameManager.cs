625fb37f2ab8fe611fcef935910eb92c09c4010b
pavani-kommana <80096838+pavani-kommana@users.noreply.github.com>
2023-02-23 22:47:08 +0530

    game completed

--- Assets/scripts/GameManager.cs
+++ Assets/scripts/GameManager.cs
@@ -13,7 +13,7 @@ public class GameManager : MonoBehaviour
     public List<Button> btn = new List<Button>();
     private bool firstGuess, secondGuess;
     private int countguess;
-    private int cointcorrectguess;
+    private int countcorrectguess;
     private int gameguess;
     private int firstguessindex, secondguessindex;
     private string firstguesspuzzle, secondguesspuzle;
@@ -28,6 +28,8 @@ public class GameManager : MonoBehaviour
         GetButton();
         AddListeners();
         AddGamepuzzle();
+        shuffle(gamepuzzle);
+        gameguess = gamepuzzle.Count / 2;
     }
     public void GetButton()
     {
@@ -62,7 +64,71 @@ public class GameManager : MonoBehaviour
     }
     void pickPuzzle()
     {
-        string name = UnityEngine.EventSystems.EventSystem.current.currentSelectedGameObject.name;
-        print("hey");
+       // string name = UnityEngine.EventSystems.EventSystem.current.currentSelectedGameObject.name;
+        if(!firstGuess)
+        {
+            firstGuess = true;
+            firstguessindex = int.Parse(UnityEngine.EventSystems.EventSystem.current.currentSelectedGameObject.name);
+            firstguesspuzzle = gamepuzzle[firstguessindex].name;
+
+            btn[firstguessindex].image.sprite = gamepuzzle[firstguessindex];
+        }
+        else if(!secondGuess)
+            {
+            secondGuess = true;
+            secondguessindex = int.Parse(UnityEngine.EventSystems.EventSystem.current.currentSelectedGameObject.name);
+            secondguesspuzle = gamepuzzle[secondguessindex].name;
+            btn[secondguessindex].image.sprite = gamepuzzle[secondguessindex];
+            if(firstguesspuzzle == secondguesspuzle)
+            {
+                print("matched");
+            }
+            else
+            {
+                print("not matched");
+            }
+            StartCoroutine(checkthepuzzlematch());
+        }
+    }
+    IEnumerator checkthepuzzlematch()
+    {
+        yield return new WaitForSeconds(0.5f);
+
+        if (firstguesspuzzle==secondguesspuzle)
+        {
+            yield return new WaitForSeconds(0.5f);
+            btn[firstguessindex].interactable = false;
+            btn[secondguessindex].interactable =false;
+
+            btn[firstguessindex].image.color= new Color(0,0,0,0);
+            btn[secondguessindex].image.color = new Color(0, 0, 0, 0);
+        }
+        else
+        {
+            btn[firstguessindex].image.sprite = bgImage;
+            btn[secondguessindex].image.sprite = bgImage;
+        }
+        yield return new WaitForSeconds(0.5f);
+        firstGuess = secondGuess = false;
+
+    }
+    void checkthegamefinish()
+    {
+        countcorrectguess++;
+        if(countcorrectguess==gameguess)
+        {
+            print(" ");
+        }
+    }
+    void shuffle(List<Sprite> list)
+    {
+        for(int i = 0;i< list.Count; i++)
+        {
+            Sprite temp=    list[i];
+            int randomIndex = Random.Range(0, list.Count);
+            list[i]=list[randomIndex];
+            list[randomIndex]=temp;
+
+        }
     }
 }
